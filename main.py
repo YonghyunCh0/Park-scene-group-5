aac24d83ca3633143b6d2a04b86d3e75cb2b50e7
YonghyunCh0 <567405@my.cuhsd.org>
2025-11-18 08:47:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import turtle as trtl
 
+# screen setup
+wn = trtl.Screen()
+wn.setup(width = 750, height= 600)
+wn.bgcolor("skyblue")
+
+# basic layout of park
+layout = trtl.Turtle()
+layout.penup()
+layout.goto(-375-300)
+layout.pendown()
+layout.left(90)
+layout.forward(350)
+layout.right(90)
+layout.forward(750)
+layout.right(90)
+layout.forward(350)
+
+wn.mainloop()
